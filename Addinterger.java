b40c1ae6350bb5de7a7dd8cabc35a1cf8fd017f9
priyasridhar10 <160344747+priyasridhar10@users.noreply.github.com>
2024-02-19 10:44:55 +0530

    Add files via upload

---
+++ Addinterger.java
@@ -0,0 +1,19 @@
+package javaintrotask;
+
+public class Addinterger {
+	
+	public static void main(String args[])
+	{	
+		 int a=4,b=5,c=8,d=1;
+	     int e=a+b; 
+	     int f=c+d;
+	
+		if (e>=f)
+		{
+			System.out.println("Sum of a and B is Greater ");
+		}
+		else
+		System.out.println(" Sum of c and d is greater");
+
+}
+}
